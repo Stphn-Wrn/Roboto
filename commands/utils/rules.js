cbf8a338c8bb7db5f09528d8471419deae031c85
Stephen <s.woirin.dev@gmail.com>
2022-06-14 11:47:42 +0200

    update

--- commands/utils/rules.js
+++ commands/utils/rules.js
@@ -18,8 +18,8 @@ const buttons = new MessageActionRow()
 
   const rulesEmbed = new MessageEmbed()
   .setTitle('Acceptez le règlement pour accéder à l\'intégralité du serveur')
-   .setDescription(`✨ - Comportement.\n\n - Restez courtois, poli. Vous pouvez être familier, nous ne vous demandons pas d’écrire comme Molière, nous ne sommes pas à L’Élysée.\n\n - Pas de violence verbale gratuite. Vous pouvez taquiner gentiment sans aller dans l’extrême. Si cela reste dans la bonne humeur et le second degré nous le tolérons. Si le staff ou moi même estimons que cela ne respecte plus la règle, vous risquez un kick ou un ban en fonction de l’humeur de la personne qui s\'occupe de votre cas (en général c’est un ban direct avec moi)\n\n ✨ – Chat écrit/vocal.\n\n  - Ne doit pas être ressemblant/confondu avec celui d’un membre du staff, sous peine d’avertissement puis ban si l’avertissement n’est pas pris en compte.\n\n - Ne doit pas contenir de propos racistes, homophobes, sexistes …sous peine d’avertissement puis ban si l’avertissement n’est pas pris en compte.\n\n  - Ne doit pas avoir de caractère pornographique, sous peine d’avertissement puis ban si l’avertissement n’est pas pris en compte.\n\n ✨ - Contacter le staff.\n\n - Si pour une quelconque raison, vous voulez contacter un membre du staff (modo ou admin), mentionnez un des <@&951468958295408710> dans le channel <#950379999754588160>`)
-   .setFooter({ text: '- Veuillez bien lire ce règlement puis validez pour avoir automatiquement votre rôle et l\'accès à tout les salons ❤️'})
+   .setDescription(`Michel`)
+   .setFooter({ text: '-Michel !'})
   .setTimestamp()
 
 module.exports = {
--- commands/utils/social-link.js
+++ commands/utils/social-link.js
@@ -3,50 +3,26 @@ const { runInteraction } = require('./help');
 
 const buttons = new MessageActionRow()
   .addComponents(
-    // new MessageButton()
-    // .setCustomId('primary-button')
-    // .setLabel('Primary')
-    // .setStyle('PRIMARY'),
+    new MessageButton()
+    .setCustomId('primary-button')
+    .setLabel('Primary')
+    .setStyle('PRIMARY'),
 
-    // new MessageButton()
-    // .setCustomId('secondary-button')
-    // .setLabel('Secondary')
-    // .setStyle('SECONDARY'),
+    new MessageButton()
+    .setCustomId('secondary-button')
+    .setLabel('Secondary')
+    .setStyle('SECONDARY'),
 
-    // new MessageButton()
-    // .setCustomId('success-button')
-    // .setLabel('Success')
-    // .setStyle('SUCCESS'),
-    
-    // new MessageButton()
-    // .setCustomId('danger-button')
-    // .setLabel('Danger')
-    // .setStyle('DANGER'),
+    new MessageButton()
+    .setCustomId('success-button')
+    .setLabel('Success')
+    .setStyle('SUCCESS'),
     
     new MessageButton()
-    .setURL('https://twitch.tv/rhaykoza_')
-    .setLabel('🟣 Twitch 🟣')
-    .setStyle('LINK'),
- 
-    new MessageButton()
-    .setURL('https://www.youtube.com/user/shymreborn')
-    .setLabel('🔴 Youtube 🔴')
-    .setStyle('LINK'),
-
-    new MessageButton()
-    .setURL('https://twitter.com/Rhaykoza')
-    .setLabel('🔵 Twitter 🔵')
-    .setStyle('LINK'),
-
-    new MessageButton()
-    .setURL('https://www.tiktok.com/@yasmine_does_art')
-    .setLabel('🎵 Tiktok 🎵')
-    .setStyle('LINK'),
+    .setCustomId('danger-button')
+    .setLabel('Danger')
+    .setStyle('DANGER'),
     
-    new MessageButton()
-    .setURL('https://www.instagram.com/yasmine_does_art/')
-    .setLabel('📷 Instagram 📷')
-    .setStyle('LINK'),
   )
 
 module.exports = {
@@ -60,9 +36,9 @@ module.exports = {
     async run(client, message, args){ 
       const fetchGuild = await client.getGuild(message.guild);
       const socialChannel = client.channels.cache.get(fetchGuild.socialChannel)
-      await socialChannel.send({content : 'Pour me rejoindre sur les différents réseaux. 💕', components: [buttons]})
+      await socialChannel.send({content : '', components: [buttons]})
     },
     async runInteraction(client, interaction){
-      await interaction.reply({content : 'Cette commande est uniquement disponible avec le préfix : !', ephemeral: true})
+      await interaction.reply({content : '', ephemeral: true})
     }
 }
